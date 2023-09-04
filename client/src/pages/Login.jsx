9f71fd8da92897ce6da5e10419394bff2e3b2635
Teej <96551689+teejcoder@users.noreply.github.com>
2023-09-04 19:07:13 +1000

    Tailwind installed

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='h-screen'>
+        <h1>
+            Login Page
+        </h1>
+    </div>
+  )
+}
+
+export default Login
